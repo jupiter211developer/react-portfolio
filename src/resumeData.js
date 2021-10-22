342f7be9c79094c8917eba3c7f618298ab9b8f1c
jupiter211 <jupiter99211@outlook.com>
2021-10-22 15:13:05 +0300

    My portfolio(React)

---
+++ src/resumeData.js
@@ -0,0 +1,100 @@
+let resumeData = {
+    "imagebaseurl":"https://jupiter211developer.github.io/",
+    "name": "Jesus Villar",
+    "role": "FullStack Developer and Data Scientist",
+    "linkedinId":"Your LinkedIn Id",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, demonstrated history of working in dozens of industries. Skilled in both front-end & back-end programming. ",
+    "socialLinks":[
+        {
+          "name":"github",
+          "url":"https://github.com/jupiter211developer",
+          "className":"fa fa-github"
+        }
+      ],
+    "aboutme":"I am a fullstack developer and participated in developing an enterprise project with hundreds of developers.",
+    "address":"Spain",
+    "website":"https://jupiter211developer.github.io",
+    "education":[
+      {
+        "UniversityName":"University of Seville",
+        "specialization":"Software Engineering",
+        "MonthOfPassing":"Sep",
+        "YearOfPassing":"2009-2013",
+        "Achievements":"B.Sc"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"ForaSoft",
+        "specialization":"Senior Full Stack Developer",
+        "MonthOfLeaving":"Oct",
+        "YearOfLeaving":"2019",
+        "Achievements":"Worked on dozens of Websites & Mobile Apps for our clients - Video/Audio chatting platforms & Apps, Live Video Streaming platforms & Apps, E-learning platforms, E-commerce sites, Food Industry websites & Apps, Health/Fitness sites & Apps"
+      },
+      {
+        "CompanyName":"NRGSoft, Remote",
+        "specialization":"Web Frontend Engineer",
+        "MonthOfLeaving":"Apr",
+        "YearOfLeaving":"2014",
+        "Achievements":"Handled responsive design layouts for views. Implemented reusable components for UX using jQuery. Created a xQuery Selector generator from a HTML DOM Node and element. Created a Pop CSS editor for elements. Created a new front-end app based on React/Redux. Worked with a React/Redux environment on a major enterprise product already in production."
+      },
+      {
+        "CompanyName":"Setronica",
+        "specialization":"Junior Web Developer",
+        "MonthOfLeaving":"Jul",
+        "YearOfLeaving":"2013",
+        "Achievements":"Did advanced JavaScript functional programming. Designed a MongoDB database. Created single-page apps with  Backbone.js  and  Ember.js. Developed software using RESTful APIs with Node.js and Express.js. Handled JavaScript UX framework prototyping."
+      }
+    ],
+    "skillsDescription":"JavaScript, C++, C, Java, Node.js, Reactjs, Vue.js, Next.js, Nuxt.js, GatsBy, Django, ...",
+    "skills":[
+      {
+        "skillname":"Javascript"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"Vuejs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"His work was perfect and wonderful.",
+        "name":"H. Garcia"
+      },
+      {
+        "description":"His skill is undoubtedly special and excellent.",
+        "name":"J. Johnson"
+      },
+      {
+        "description":"I think that he's one of the most wonderful developers I've ever seen.",
+        "name":"B. Jack"
+      }
+    ]
+  }
+  
+  export default resumeData
